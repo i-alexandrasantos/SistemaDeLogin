0d168feec1d6c053bc497a951031e2eccef3d941
Alexandra Santos <aleseect@gmail.com>
2025-11-13 11:00:13 -0300

    declara variáveis iniciais e uma opção de menu

--- Program.cs
+++ Program.cs
@@ -10,7 +10,63 @@ namespace SistemaDeLogin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("::::: Sistema de Login :::::");
+
+            //criar minha conta
+            //string nome = "";
+            // string dataNascimento = "";
+            int sexo = 0;
+            string nomeUsuario = "";
+            string email = "";
+            string senha =  "";
+            string confirmacaoSenha = "";
+
+            // Inicio Criação da Conta
+
+            Console.WriteLine("Insira as informações abaixo para criar sua nova conta: ");
+            Console.WriteLine("Insira o nome completo: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"Olá, {nome}. Agora digite a data de nascimento");
+            string dataNascimento = Console.ReadLine();
+
+            // Fim Criação da Conta
+
+
+            //mudar senha
+            string novaSenha = "";
+            //// confirmação de senha
+
+            //logar
+
+
+            Console.WriteLine("::::: Bem vindo(a) ao Sistema :::::");
+            Console.WriteLine("Selecione abaixo uma opção para continuar...");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("1 - Logar no sistema");
+            Console.WriteLine("2 - Criar conta");
+            Console.WriteLine("3 - Mudar a senha");
+            Console.WriteLine("4 - Sair do sistema");
+            Console.WriteLine(" ");
+
+            int menu = Convert.ToInt32(Console.ReadLine());
+
+            switch (menu)
+            {
+                case 1:
+                    Console.WriteLine("Logado no sistema");
+                    break;
+                case 2:
+                    Console.WriteLine("Conta criada com sucesso!");
+                    break;
+                case 3:
+                    Console.WriteLine("Senha alterada!");
+                    break;
+                case 4:
+                    Console.WriteLine("Deslogado com sucesso!");
+                    break;
+            }
+
         }
     }
 }
